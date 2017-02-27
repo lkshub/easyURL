efeb927e31ee107b963b2cc0b3f32388306f6d82
Ubuntu <lk930121@easyurl-test.wl4st1hprmjuxn13t03apsc4tf.bx.internal.cloudapp.net>
2017-02-27 04:02:32 +0000

    modified port and tried run the app on Microsoft Azure

--- main.js
+++ main.js
@@ -10,13 +10,15 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 db.connectDB();
 
 app.get('/', function(req, res){
+	console.log('received index page request');
 	res.sendFile(__dirname+"/html/index.html");
 })
 app.get('/[a-z]{6}', function(req, res){
 	console.log("has the short request!");
 	db.retrieveLongURL(req.url.substring(1,7), function(result){
 		if(result.exist){
-			if(result.longURL.startsWith('http(s)?://') ){
+			console.log(result.longURL+' is in DB!');
+			if(result.longURL.indexOf("http://") == 0 || result.longURL.indexOf("https://") == 0 ){
 				console.log("the url has http!");
 				res.redirect(result.longURL);
 			}else {
@@ -37,7 +39,7 @@ app.post('/generate', urlencodedParser, function(req, res){
 	
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(80, function () {
 	    var host = server.address().address
 	    var port = server.address().port
 	    console.log("Example app listening at http://%s:%s", host, port)
