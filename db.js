b14aa113b9809c042bf8e6413d194e9ef5d043e4
Kai, Lu <lk930121@gmail.com>
2017-03-05 20:01:35 -0500

    add numbers 0-9 and Capital letters A-Z to the generated url

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ assert = require('assert');
 // Connection URL
 var url = 'mongodb://localhost/easyURL';
 var MongoClient = require('mongodb').MongoClient;
-var characters  = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w','x', 'y', 'z'];
+var characters  = ['0','1','2','3','4','5','6','7','8','9','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w','x', 'y', 'z', 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 module.exports = {
   connectDB : function(){
